b45838087150d1fe8dc35a1d558d3d2f5608ca55
Rajibbepary <rajib635356@gmail.com>
2024-12-29 23:53:55 +0600

    create custom hook usemenu data load

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover";
+import menuImg from '../../assets/menu/banner3.jpg'
 
 
 const Menu = () => {
@@ -7,6 +9,9 @@ const Menu = () => {
             <Helmet>
                 <title>Bisto Boss | Menu</title>
             </Helmet>
+            <Cover img={menuImg} title='our menu'/>
+           
+            
         </div>
     );
 };
